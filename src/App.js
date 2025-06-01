3d6e31923c03b036df030e3643700ed267c1bce0
ponz-NK <211729574+6with9@users.noreply.github.com>
2025-06-02 04:12:59 +0900

    commit done

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Sidebar from './components/Slidebar/Sidebar';
